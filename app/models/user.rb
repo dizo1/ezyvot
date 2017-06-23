a028ec0f438001ca222596187040d662e376cc9b
Muganda <mugandadizo@gmail.com>
2017-06-23 13:22:01 +0000

    facebook authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,8 @@ class User < ApplicationRecord
   end
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    #where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    where(email: auth.info.email).first_or_initialize.tap do |user|
     user.email = auth.info.email
     user.provider = auth.provider
     user.uid = auth.uid
